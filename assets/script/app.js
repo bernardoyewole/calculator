1e31070df01659b357b5313a174683bdec9b7dd7
Bernard <oyebenard@gmail.com>
2023-12-09 18:24:52 -0600

    Operands click functions

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
 import { onEvent, select, selectAll, create, print } from "./utils.js";
+
+const input = select('input')
+const operands = selectAll('.operand');
+const result = select('.result');
+const reset = select('.reset');
+const backspace = select('.backspace');
+
+operands.forEach(operand => {
+    onEvent('click', operand, () => {
+        input.value += operand.innerText;
+    });
+});
+
+onEvent('click', reset, () => {
+    input.value = '';
+    result.value = '';
+});
+
+onEvent('click', backspace, () => {
+    let len = input.value.length;
+    input.value = input.value.slice(0, len - 1);
+});
